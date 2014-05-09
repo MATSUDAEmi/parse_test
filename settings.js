90f840e512ee6506838f0d6b9a580ca2f7617ce5
ariyo13 <ariyo1331@gmail.com>
2014-05-09 19:18:48 +0900

    とりあえず必要なパーツ

---
+++ settings.js
@@ -0,0 +1,15 @@
+exports.parseAppId = 'AWGwqZGRVAyHP3TEwSKBG5FuoB08saavcJjxLuoM';
+exports.parseRestKey = '8jeYFitqu11bIhbKoaiNrOm2Usm2K9BEXEKetiuo';
+exports.mailHost = 'smtp.gmail.com';
+exports.mailPort = 465;
+exports.mailSenderAdd = 'ariyo1331@gmail.com';
+exports.mailSenderPass = '1360hn1360';
+exports.mailFrom = 'ariyo1331@gmail.com';
+exports.mailSubject = ' タイトル';
+exports.mailText0 = ' 本文１';
+exports.mailText1 = '本文２';
+
+exports.createClass = 'SettingClasses';
+
+
+exports.port = 3000;
